0117d86d82af196361e51a38d2cf74cd1238e00b
kaviru <hapuarachchikaviru@gmail.com>
2024-02-22 23:26:57 +0530

    profile

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -16,12 +16,15 @@ export default function Unauthorized() {
     }, [status])
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <div className="max-w-md mx-auto bg-gray-800 rounded-lg shadow-md overflow-hidden p-24 m-8">
-                <h1 className="text-2xl font-bold mb-4">Unauthorized</h1>
+        <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 ">
+            <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden p-6 sm:p-8 m-4 sm:m-8">
+                <div className="bg-red-500 text-white flex items-center  justify-between px-4 py-3 rounded mb-4">
+                    <h1 className="text-2xl font-bold mb-4">Unauthorized</h1>
+                </div>
                 <p>You are not authorized to access this page.</p>
             </div>
             <LoginButton />
         </main>
+
     );
 };

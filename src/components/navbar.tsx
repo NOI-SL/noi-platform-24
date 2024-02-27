6deb1ee23de02910749d33bb5c359ed437dcd256
kaviru <hapuarachchikaviru@gmail.com>
2024-02-27 23:10:30 +0530

    profile link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -120,7 +120,7 @@ const Navbar: React.FC<NavbarProps> = (NavbarProps) => {
                                             <NavigationMenuLink asChild>
                                                 <Link
                                                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                                    href="/" target="_blank"
+                                                    href="/profile"
                                                 >
                                                     {NavbarProps.status === "authenticated" ? (
                                                         <div>

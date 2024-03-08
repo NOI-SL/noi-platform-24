612739a7c8f457e44496ecc9c88984af9ccb43e8
kaviru <hapuarachchikaviru@gmail.com>
2024-03-08 12:36:21 +0530

    changes to the loading screen

--- src/components/loadingscreen.tsx
+++ src/components/loadingscreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { AiOutlineLoading } from 'react-icons/ai'; // Import AiOutlineLoading icon
+
+
 
 const LoadingScreen: React.FC = () => {
     return (
         <div className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
             <div className="flex flex-col items-center">
-                <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24 mb-4"></div>
+                <AiOutlineLoading className="w-24 h-24 mb-4 text-white" /> {/* Use the imported icon */}
                 <p className="text-white">Loading...</p>
             </div>
         </div>

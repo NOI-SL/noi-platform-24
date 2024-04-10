7febd9c4998f38d8e3e30f7640c4930102398c5a
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-04-10 14:17:33 +0530

    Before Add Dropzone

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -462,15 +462,6 @@ const Page: React.FC = () => {
             style={{ display: display.success ? "block" : "none" }}
           >
             <div className="flex items-center justify-center">
-              <svg
-                className="flex-shrink-0 w-4 h-4 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-              </svg>
               <span className="sr-only">Info</span>
               <h3 className="text-lg font-medium">Registration Successful</h3>
             </div>
@@ -490,15 +481,6 @@ const Page: React.FC = () => {
                 type="button"
                 className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
-                <svg
-                  className="me-2 h-3 w-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 14"
-                >
-                  <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
-                </svg>
                 <Link
                   className="z-21"
                   target="_blank"
@@ -507,22 +489,6 @@ const Page: React.FC = () => {
                   href="https://chat.whatsapp.com/JL2qyoImHrm4rqOYybeZTB"
                 >
                   <div>Join WhatsApp Group</div>
-                </Link>
-              </button>
-              <button
-                type="button"
-                className="text-green-800 bg-transparent border border-green-800 hover:bg-green-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-green-600 dark:border-green-600 dark:text-green-400 dark:hover:text-white dark:focus:ring-green-800"
-                data-dismiss-target="#alert-additional-content-3"
-                aria-label="Close"
-              >
-                <Link
-                  className="z-21"
-                  href="/"
-                  target="_blank"
-                  style={{ zIndex: 21 }}
-                  rel="noopener noreferrer"
-                >
-                  <div>Back to Home</div>
                 </Link>
               </button>
             </div>

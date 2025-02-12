4b178dcd43c1df1c62c9eb9da494039dcdeb2db7
lakshith <lakshith.k.nishshanke@gmail.com>
2025-02-12 22:17:09 +0530

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,30 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import logo from "../../public/goldlogoonly.png";
-import { NextAuthProvider } from './providers'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import logo from "../../public/logo.png"
+import { NextAuthProvider } from "./providers"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "NOI 25 | National Olympiad in Informatics 25 ",
-  description: "The official website of the National Olympiad in Informatics Sri Lanka 2025",
-};
+    title: "NOI 25 | National Olympiad in Informatics 25 ",
+    description:
+        "The official website of the National Olympiad in Informatics Sri Lanka 2025",
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href={logo.src} />
-      </head>
-      <body className={inter.className}>
-        <NextAuthProvider>
-          {children}
-        </NextAuthProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                <link rel="icon" href={logo.src} />
+            </head>
+            <body className={inter.className}>
+                <NextAuthProvider>{children}</NextAuthProvider>
+            </body>
+        </html>
+    )
 }

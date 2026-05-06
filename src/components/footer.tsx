99827f00bdbeb8bad7dafe91b6564dc5319e8d48
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2026-05-06 10:09:02 +0530

    feat: add site-wide footer component with branding, navigation links, and social media connectivity

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
-import logo from "./NOILogoGold.svg"
+import logo from "../../public/logo.png"
 
 const Footer: React.FC = () => {
     return (
